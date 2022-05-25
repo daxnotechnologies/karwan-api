03bbb24c6c1ae4b429301532ff1021f391d26ea8
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-05-25 17:56:30 +0500

    make it1

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const DB = process.env.MONGO_URI;
-
+// const DB = process.env.MONGO_URI;
+const DB =
+  "mongodb+srv://naveed:naveedAHMED12234@cluster0.idy7f.mongodb.net/users?retryWrites=true&w=majority";
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
