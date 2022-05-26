1c9dd40f06420a9b1366beb17fa1732bad42ea15
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-05-26 19:08:05 +0500

    express-json-limit10mb

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "100000kb" }));
 
 app.use(userRouter);
 app.use(videoRouter);
