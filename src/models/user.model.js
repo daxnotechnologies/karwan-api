780d76dfe9ca0c541c8e646f01dbe1983df1a2fa
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-05-26 16:14:52 +0500

    contact-string

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true },
-  contact: { type: Number, required: true },
+  contact: { type: String, required: true },
   address: { type: String, required: true },
   profilePic: { type: String, required: false },
   password: { type: String, required: true },
