db699326f92daa577eb8c50a1b85569fcebd9407
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-06-02 17:05:13 +0500

    banner

--- src/models/banner.model.js
+++ src/models/banner.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const BannerSchema = new mongoose.Schema({
+  title: { type: String, required: true },
   bannerImage: { type: String, required: true },
 });
 
